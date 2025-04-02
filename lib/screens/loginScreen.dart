d86116c37edbf4f6379b2c7dce80aaa0ddd7cadd
diaa31412 <dhariri31412@gmail.com>
2025-04-03 02:10:33 +0300

    Complete login page

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/textFieldInput.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -10,6 +11,16 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +31,63 @@ class _LogInScreenState extends State<LogInScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(), flex: 2),
               SvgPicture.asset(
-                'assets/ic_inatagram.svg',
+                'assets/ic_instagram.svg',
                 color: primaryColor,
                 height: 64,
+              ),
+              SizedBox(height: 64),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: "Enter Your Email",
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 24),
+              TextFieldInput(
+                textEditingController: _passController,
+                hintText: "Enter Your Password",
+                textInputType: TextInputType.emailAddress,
+                isPass: true,
+              ),
+              SizedBox(height: 24),
+              InkWell(
+                child: Container(
+                  child: Text('Login'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+              SizedBox(height: 12),
+              Flexible(child: Container(), flex: 2),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: Text("Don't have an account?"),
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      child: Text(
+                        "Sign up.",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
